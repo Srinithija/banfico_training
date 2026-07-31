2df864960c8548fa636a372915c94c5a4e1ed9aa
Srinithija <srinithijasivakumar@gmail.com>
2026-07-31 19:57:22 +0530

    git api info

--- src/main/java/com/apiconnection/apiconnection/controller/InfoController.java
+++ src/main/java/com/apiconnection/apiconnection/controller/InfoController.java
@@ -1,7 +1,13 @@
 package com.apiconnection.apiconnection.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @RestController
 public class InfoController {
@@ -12,8 +18,19 @@ public class InfoController {
     }
 
     @GetMapping("/api/info")
-    public String info() {
-        return "Spring Boot API Service";
-    }
+    public Map<String, String> info() throws Exception {
 
+        Properties properties = new Properties();
+
+        InputStream input = new ClassPathResource("git.properties").getInputStream();
+        properties.load(input);
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("branch", properties.getProperty("git.branch"));
+        response.put("commitId", properties.getProperty("git.commit.id.abbrev"));
+        response.put("commitMessage", properties.getProperty("git.commit.message.short"));
+
+        return response;
+    }
 }
